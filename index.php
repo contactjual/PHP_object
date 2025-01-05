227661d709351052a94184e725bc0133500b5f7a
Jual Rana <contactjual@gmail.com>
2025-01-05 10:48:41 +0600

    construc and desstruct

--- index.php
+++ index.php
@@ -26,10 +26,48 @@ class man {
     function details ($fname, $years) {
         $this->name = $fname;
         $this->age = $years;
-        return "$this->name's age is $this->age years";
+        return "$this->name's age is $this->age years <br>";
     }
 }
 $man_obj = new man;
 echo $man_obj -> details("Jual", 19);
+
+
+
+
+// __construct funciton ....................... [auto exicute]
+
+// class human {
+//     public $name;
+//     public $age;
+//     function __construct ($name, $age) {
+//         $this->name = $name;
+//         $this->age = $age;
+//     }
+//     function info () {
+//         return "name: $this->name and age: $this->age <br>";
+//     }
+// }
+// $human_obj = new human("Ratul", 30);
+// echo $human_obj-> info ();
+
+
+
+// __destruct funciton .......................[auto exicute]
+
+class human {
+    public $name;
+    public $age;
+    function __construct ($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+    function __destruct () {
+        echo "name: $this->name and age: $this->age <br>";
+    }
+}
+$human_obj = new human("Ratul", 30);
+    
+
 
 ?>
